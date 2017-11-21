0de31b51397fc8b9cd0e93ec2a707a13d185668d
Samweli <smwltwesa6@gmail.com>
2017-11-21 12:42:49 +0300

    returning plain text

--- web/index.php
+++ web/index.php
@@ -59,7 +59,7 @@ $app->post('/', function() use($app) {
     // Print the response onto the page so that our gateway can read it
     header('Content-type: text/plain');
 
-    return "CON What would you want to check \n";
+    return $response;
 // DONE!!!
 
 });

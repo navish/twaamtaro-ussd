47ad7b06ff8c14cdac215dcf93ad725c21f42188
Samweli <smwltwesa6@gmail.com>
2017-11-22 14:22:35 +0300

    updated on how to check a number

--- web/index.php
+++ web/index.php
@@ -26,82 +26,84 @@ $app->post('/', function() use($app) {
     $phonenumber = $_POST["phoneNumber"]; //Assumed mobile number
     $serviceCode = $_POST["serviceCode"];
 
-
-        //$sessionId=$_GET['sessionId']; //For any audits/checks
-        $dbcon = db(); 
-        $level = explode("*", $res);
-        if (isset($res)) {
-        $number =  $phonenumber;
-        $citizen = pg_query($dbcon, "SELECT * FROM users WHERE sms_number='$number'");
-
-        if (pg_num_rows($citizen) > 0) {
-            $user_row = pg_fetch_assoc($citizen);
-            $user = $user_row['id'];
-            $mhusika = $user_row['first_name'].' '.$user_row['last_name'];
-            $role = $user_row['role'];            
-   
-        if ( $res == "" ) {
-            $response = dWelcomeMenu($mhusika); 
-        }
-        if(isset($level[0]) && $level[0]!= "" && !isset($level[1])){
-            switch ($level[0]) {
-                case 1:
-                   $response = dGetInfoMenu();
-                break;
-                case 2:
-                    $response = dSendInfoMenu();
-                break;
-                case 3:
-                    $response = askForHelp($user);
-                    
-                break;
-                case 4:
-                    $response = switchLang();
-                break;
-                
-                default:
-                     $response = "CON Chaguo sio sahihi\n";
-                break;
-            }
-             
-        }
-        else if(isset($level[1]) && $level[1]!="" && $level[0]=="1"  && !isset($level[2])){
-            switch ($level[1]) {
-                case 1:
-                   $response = getDrainStatus($user);
-                break;
-                case 2:
-                    $response = getCollaborators($user);
-                break;    
-                default:
-                     $response = "CON Chaguo sio sahihi\n";
-                break;
-            }   
-            
-        }
-        else if(isset($level[1]) && $level[1]!="" && $level[0]=="2" && !isset($level[2])){
-            $response = sendInfo($level[1],$user);
-        }
-        
-        else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
-            //Save data to database
-         echo("There is nothing here");
-            
-            $response = "END Asante kwa kutumia huduma yetu";
-        }
-        
-        }//If citizen is registered
-
-        else { 
-
-          $response = "CON Namba yako, ".$number." haijasajiliwa na Twaa Mtaro. Wasiliana na kiongozi wako wa mtaa kwa msaada zaidi "; 
+    //$sessionId=$_GET['sessionId']; //For any audits/checks
+    $dbcon = db(); 
+    $level = explode("*", $res);
+    if (isset($res)) {
+      if (strpos($number, '+') !== false) {
+          $number = str_replace("+","",$phonenumber);
       }
-        header('Content-type: res/plain');
-        return $response;
-        }//If USSD String is set
+      
+      $citizen = pg_query($dbcon, "SELECT * FROM users WHERE sms_number='$number'");
 
-    header('Content-type: text/plain');
+    if (pg_num_rows($citizen) > 0) {
+        $user_row = pg_fetch_assoc($citizen);
+        $user = $user_row['id'];
+        $mhusika = $user_row['first_name'].' '.$user_row['last_name'];
+        $role = $user_row['role'];            
+
+    if ( $res == "" ) {
+        $response = dWelcomeMenu($mhusika); 
+    }
+    if(isset($level[0]) && $level[0]!= "" && !isset($level[1])){
+        switch ($level[0]) {
+            case 1:
+               $response = dGetInfoMenu();
+            break;
+            case 2:
+                $response = dSendInfoMenu();
+            break;
+            case 3:
+                $response = askForHelp($user);
+                
+            break;
+            case 4:
+                $response = switchLang();
+            break;
+            
+            default:
+                 $response = "CON Chaguo sio sahihi\n";
+            break;
+        }
+         
+    }
+    else if(isset($level[1]) && $level[1]!="" && $level[0]=="1"  && !isset($level[2])){
+        switch ($level[1]) {
+            case 1:
+               $response = getDrainStatus($user);
+            break;
+            case 2:
+                $response = getCollaborators($user);
+            break;    
+            default:
+                 $response = "CON Chaguo sio sahihi\n";
+            break;
+        }   
+        
+    }
+    else if(isset($level[1]) && $level[1]!="" && $level[0]=="2" && !isset($level[2])){
+        $response = sendInfo($level[1],$user);
+    }
+    
+    else if(isset($level[2]) && $level[2]!="" && !isset($level[3])){
+        //Save data to database
+     echo("There is nothing here");
+        
+        $response = "END Asante kwa kutumia huduma yetu";
+    }
+    
+    }//If citizen is registered
+
+    else { 
+
+      $response = "END Namba yako, ".$number." haijasajiliwa na Twaa Mtaro. Wasiliana na kiongozi wako wa mtaa kwa msaada zaidi "; 
+  }
+    header('Content-type: res/plain');
     return $response;
+    }//If USSD String is set
+
+header('Content-type: text/plain');
+return $response;
 // DONE!!!
 
 });

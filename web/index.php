8eac808e7c455eb88737e8221093313fb24db956
Samweli <smwltwesa6@gmail.com>
2017-11-24 12:05:39 +0300

    update on drainstatus response

--- web/index.php
+++ web/index.php
@@ -70,7 +70,7 @@ $app->post('/', function() use($app) {
     else if(isset($level[1]) && $level[1]!="" && $level[0]=="1"  && !isset($level[2])){
         switch ($level[1]) {
             case 1:
-               $sqlClaims = pg_query($dbcon, "SELECT * FROM drain_claims WHERE user_id=$userId");
+               $sqlClaims = pg_query($dbcon, "SELECT * FROM drain_claims WHERE user_id=$user");
                $response = getDrainStatus($sqlClaims);
             break;
             case 2:

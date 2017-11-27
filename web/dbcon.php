f2f821da0ef38340fe86eb468330c0924ce47bcb
Navish <navish45@gmail.com>
2017-11-27 20:01:55 +0300

    extended the need help menu

--- web/dbcon.php
+++ web/dbcon.php
@@ -7,9 +7,8 @@ function db()
 }
 
 function pg_connection_string_from_database_url() {
- // extract(parse_url($_ENV["DATABASE_URL"]));
-  //return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-  return "user=postgres password=nancy host=localhost dbname=mitaro";
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
     
 ?>

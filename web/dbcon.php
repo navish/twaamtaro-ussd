91715046f0ccf05c66c57978af75916e08fc29b9
Samweli <smwltwesa6@gmail.com>
2017-12-05 17:34:24 +0300

    added host in db con

--- web/dbcon.php
+++ web/dbcon.php
@@ -8,6 +8,6 @@ function db()
 
 function pg_connection_string_from_database_url() {
   extract(parse_url('postgres://jplhkyofzqmllx:69565a9e84a5e350ca1ded9f75004924ec08cd2d7874b16417230763061e7cac@ec2-23-23-78-213.compute-1.amazonaws.com:5432/d8dsrp7ftqpl8'));
-  return 'user=$user password=$pass host=ec2-23-23-78-213.compute-1.amazonaws.com dbname=' . substr($path, 1); # <- you may want to add sslmode=require there too
+  return 'user=jplhkyofzqmllx password=69565a9e84a5e350ca1ded9f75004924ec08cd2d7874b16417230763061e7cac host=ec2-23-23-78-213.compute-1.amazonaws.com dbname=d8dsrp7ftqpl8'; # <- you may want to add sslmode=require there too
 }
 ?>

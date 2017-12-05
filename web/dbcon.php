723d4b1c415bab3e09cae674e46bba146506bd87
Samweli <smwltwesa6@gmail.com>
2017-12-05 17:28:01 +0300

    added host in db con

--- web/dbcon.php
+++ web/dbcon.php
@@ -8,6 +8,6 @@ function db()
 
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV['DATABASE_URL']));
-  return 'user=$user password=$pass host=$host dbname=' . substr($path, 1); # <- you may want to add sslmode=require there too
+  return 'user=$user password=$pass host=ec2-23-23-78-213.compute-1.amazonaws.com dbname=' . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 ?>

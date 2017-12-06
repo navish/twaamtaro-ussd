6a60f0b0c798576eb45596d9657ccaf9e7782771
Navish <navish45@gmail.com>
2017-12-06 15:36:30 +0300

    fix on send info level

--- web/index.php
+++ web/index.php
@@ -103,7 +103,7 @@ $app->post('/', function() use($app) {
             
         }
         else if(isset($level[1]) && $level[1]!="" && $level[0]=="2" && !isset($level[2])){
-            $response = sendInfo($level[2],$user,$lang);
+            $response = sendInfo($level[1],$user,$lang);
         }
         
         else if(isset($level[0]) && $level[0]!="" && $level[0]=="3"){
